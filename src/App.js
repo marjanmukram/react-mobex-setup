80b67065b4a28645905993aeebe99d95b45e0f64
Marjan Mukram <marjan@Mahmoods-MacBook-Air.local>
2019-11-16 12:02:26 +0530

    MOBEX INITAIL  SETUP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Store from "./Store";
+import Form from "./components/Form";
+import Dashboard from "./components/Dashboard";
+import Reviews from "./components/Reviews";
+import { decorate, observable, action, computed } from "mobx";
+
+decorate(Store, {
+  reviewList: observable,
+  addReview: action,
+  averageScore: computed,
+  reviewCount: computed
+});
+
+const reviewStore = new Store();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form store={reviewStore} />
+      <Dashboard store={reviewStore} />
+      <Reviews store={reviewStore} />
     </div>
   );
 }
